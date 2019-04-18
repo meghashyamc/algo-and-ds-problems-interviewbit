c591ee29d812ee4255cc50c24b3c49862e71fede
Meghashyam Chirravoori <c.meghashyam@gmail.com>
2019-04-18 22:21:03 +0530

    Update and rename minSumPath.java to MinSumPath.java

--- Dynamic Programming../MinSumPath.java
+++ Dynamic Programming../MinSumPath.java
@@ -1,3 +1,18 @@
+/* Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+ Note: You can only move either down or right at any point in time. 
+Example :
+
+Input : 
+
+    [  1 3 2
+       4 3 1
+       5 6 1
+    ]
+
+Output : 8
+     1 -> 3 -> 2 -> 1 -> 1 8 */
+
 import java.util.ArrayList;
 
 public class MinSumPath {
