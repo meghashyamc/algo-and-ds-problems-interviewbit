d2b37ba8a25d45cdbbc2ef9a0038c412913d76cf
Meghashyam Chirravoori <c.meghashyam@gmail.com>
2019-06-17 08:22:34 +0530

    Add files via upload

---
+++ heapsMaps/DistinctNosWindows.java
@@ -0,0 +1,111 @@
+package test.heapsMaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/*
+
+You are given an array of N integers, A1, A2 ,…, AN and an integer K. Return the of count of distinct numbers in all windows of size K.
+
+Formally, return an array of size N-K+1 where i’th element in this array contains number of distinct elements in sequence Ai, Ai+1 ,…, Ai+k-1.
+
+Note:
+
+If K > N, return empty array.
+For example,
+
+A=[1, 2, 1, 3, 4, 3] and K = 3
+
+All windows of size K are
+
+[1, 2, 1]
+[2, 1, 3]
+[1, 3, 4]
+[3, 4, 3]
+
+So, we return an array [2, 3, 3, 2].
+ */
+
+public class DistinctNosWindows {
+
+    public ArrayList<Integer> dNums(ArrayList<Integer> A, int B) {
+
+        int n = A.size();
+
+        // this is the answer array
+        ArrayList<Integer> nosInWindows = new ArrayList<>();
+
+        if (B > n) return nosInWindows;
+
+        // numOccs stores the number of occurences of each number
+        HashMap<Integer, Integer> numOccs = new HashMap<>();
+
+        // we are going till n-B+1 since that is the number of B sized groups we get when there
+        // are n numbers in total
+        for(int i = 0; i < n-B+1;i++ ){
+
+            // get next window of B numbers
+            List<Integer> currList = A.subList(i, i+B);
+
+            // the first window
+            if (i == 0) {
+                for (Integer currInt : currList) {
+                    // this increments the number of occurences of that number by 1
+                    addToMap(numOccs, currInt);
+                }
+
+                // the size of numOccs hashmap is the number of distinct numbers in the current
+                // window
+                nosInWindows.add(numOccs.size());
+            }
+
+            else{
+
+                // as we move to the next window, we remove the first
+                // number from previous window and add the next number to the window
+                removeFromMap(numOccs, A.get(i-1));
+                addToMap(numOccs, A.get(i+B-1));
+                // the size of numOccs hashmap is the number of distinct numbers in the current
+                // window
+                nosInWindows.add(numOccs.size());
+            }
+        }
+
+        return nosInWindows;
+
+
+    }
+
+    private void addToMap(HashMap<Integer, Integer> map, int i){
+
+        if(!map.containsKey(i)) map.put(i, 1);
+        else map.put(i, map.get(i)+1);
+    }
+
+
+    private void removeFromMap(HashMap<Integer, Integer> map, int i){
+
+        if(!map.containsKey(i)) return;
+        else {
+
+            if (map.get(i) == 1) map.remove(i);
+            else map.put(i, map.get(i)-1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DistinctNosWindows distinctNosWindows = new DistinctNosWindows();
+
+        ArrayList<Integer> a = new ArrayList<>();
+
+        int[] arr = {1, 2, 1, 3, 4, 3};
+
+        for(int i = 0; i < arr.length; i++) a.add(arr[i]);
+
+        System.out.println(distinctNosWindows.dNums(a, 1));
+
+
+    }
+}
---
+++ heapsMaps/LRUCache.java
@@ -0,0 +1,147 @@
+package test.heapsMaps;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class LRUCache {
+
+private int overallTimeCount = 0;
+private int limit;
+private PriorityQueue<Key> pq;
+private HashMap<Integer, Integer> keyValsMap;
+private HashMap<Integer, Key> keyKeyMap;
+
+    public LRUCache(int capacity) {
+
+    // priority queue stores keys (a data structure that has a key and its latest timestamp)
+        // it gives us the key with lowest timestamp (accessed earliest) when asked
+      pq = new PriorityQueue<>(new Comparator<Key>() {
+            @Override
+            public int compare(Key k1, Key k2) {
+                return k1.timeCount - k2.timeCount;
+            }
+        });
+
+      // hashmap that stores a key and it's key-timestamp combo structure
+      keyKeyMap = new HashMap<>();
+
+      // hashmap that stores the keys and values we need to store
+      keyValsMap = new HashMap<>();
+
+
+        limit = capacity;
+
+
+
+    }
+
+    private static class Key{
+
+        int key;
+        int timeCount;
+
+        public Key(int key1, int timeCount1){
+            key = key1;
+            timeCount = timeCount1;
+        }
+
+        @Override
+        public String toString() {
+
+            StringBuilder str = new StringBuilder();
+
+            str.append("key: " + this.key + " ");
+            str.append("TimeCount: " + this.timeCount);
+            return str.toString();
+        }
+    }
+
+    public int get(int key) {
+
+        // the key doesn't exist in our table
+        if (!keyValsMap.containsKey(key)) return -1;
+        else {
+
+            //get the key-timestamp combo
+            Key k = keyKeyMap.get(key);
+            // remove the key-timestamp-combo we just got
+            pq.remove(k);
+            // add a new key-timestamp combo with new time to priority queue
+            k.timeCount = ++overallTimeCount;
+            pq.add(k);
+            // return the value corresponding to the key
+            return keyValsMap.get(key);
+        }
+
+
+    }
+
+    public void set(int key, int value) {
+
+        // the key doesn't exist in our table yet
+
+        if (!keyValsMap.containsKey(key)) {
+            // have we reached our limit?
+            if (pq.size() == limit) {
+
+                // remove the key with the earliest timestamp from pq
+                Key byeKey = pq.peek();
+                pq.remove(byeKey);
+                // remove that key from both hashmaps
+                keyKeyMap.remove(byeKey.key);
+                keyValsMap.remove(byeKey.key);
+            }
+
+            // add new key/ key-timestamp combo to both hashmaps/priority queue
+            Key k = new Key(key, ++overallTimeCount);
+            keyKeyMap.put(key, k);
+            keyValsMap.put(key, value);
+            pq.add(k);
+
+        }
+
+        // the key already exists in our table
+        else{
+
+            // remove key-timestamp combo from priority queue, update
+            // timestamp and add it to priority queue again
+            Key k = keyKeyMap.get(key);
+            pq.remove(k);
+
+            k.timeCount = ++overallTimeCount;
+            pq.add(k);
+
+            // add key and corresponding value to hashmap (the key-value one)
+            keyValsMap.put(key, value);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+LRUCache lruCache = new LRUCache(2);
+
+        lruCache.set(1, 10);
+        System.out.println("pq is " + lruCache.pq);
+        lruCache.set(5, 12);
+        System.out.println("pq is " + lruCache.pq);
+
+       System.out.println(lruCache.get(5));
+        System.out.println("pq is " + lruCache.pq);
+
+        System.out.println(lruCache.get(1));
+        System.out.println("pq is " + lruCache.pq);
+
+        System.out.println(lruCache.get(10));
+        System.out.println("pq is " + lruCache.pq);
+
+        lruCache.set(6, 14);
+
+        System.out.println("pq is " + lruCache.pq);
+        System.out.println(lruCache.get(5));
+        System.out.println("pq is " + lruCache.pq);
+
+
+    }
+}
