c0d36918c3c1d5f94c8b7954b90dac7612f22242
meghashyamc <c.meghashyam@gmail.com>
2022-10-20 08:44:40 +0530

    minor update to readme and a package-name-fix for a file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Some problems related to algorithms and data structures from [Interview Bit](https://www.interviewbit.com/courses/programming/)
+# Problems related to algorithms and data structures from [Interview Bit](https://www.interviewbit.com/courses/programming/)
 
 
-Each `.java` file has a description of the problem and then the solution.
+Each `.java` file has (or is at least supposed to have) a description of the problem and then the solution.
--- src/arrays/LargestNumber.java
+++ src/arrays/LargestNumber.java
@@ -1,4 +1,4 @@
-package test.arrays;
+package arrays;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,64 +7,64 @@ import java.util.List;
 
 public class LargestNumber {
 
-    // this comparator sorts numbers in lexicographic order (after first
-    // converting them to Strings)
-    private class specialSort implements Comparator<Integer>{
+	// this comparator sorts numbers in lexicographic order (after first
+	// converting them to Strings)
+	private class specialSort implements Comparator<Integer> {
 
-public int compare(Integer i1, Integer i2){
+		public int compare(Integer i1, Integer i2) {
 
-    String str1 = (i1.toString() + i2.toString());
-    String str2 = (i2.toString() + i1.toString());
+			String str1 = (i1.toString() + i2.toString());
+			String str2 = (i2.toString() + i1.toString());
 
-  Long cmp = (Long.parseLong(str1) - Long.parseLong(str2));
+			Long cmp = (Long.parseLong(str1) - Long.parseLong(str2));
 
-  if (cmp > 0) return 1;
-  else if (cmp < 0) return -1;
-  else return 1;
+			if (cmp > 0)
+				return 1;
+			else if (cmp < 0)
+				return -1;
+			else
+				return 1;
 
-    }
+		}
 
-}
+	}
 
-    // DO NOT MODIFY THE LIST. IT IS READ ONLY
-    public String largestNumber(final List<Integer> A) {
+	// DO NOT MODIFY THE LIST. IT IS READ ONLY
+	public String largestNumber(final List<Integer> A) {
 
-        ArrayList<Integer> a = new ArrayList<>(A);
+		ArrayList<Integer> a = new ArrayList<>(A);
 
 // what we want is simply the numbers arranged in reverse lexicographic order
-Comparator<Integer> c = Collections.reverseOrder(new specialSort());
+		Comparator<Integer> c = Collections.reverseOrder(new specialSort());
 
-Collections.sort(a, c);
+		Collections.sort(a, c);
 
-        StringBuilder str = new StringBuilder();
+		StringBuilder str = new StringBuilder();
 
-        if (a.get(0).equals(0)) return "0";
+		if (a.get(0).equals(0))
+			return "0";
 
-else {
-    for(Integer i: a) str.append(i);
+		else {
+			for (Integer i : a)
+				str.append(i);
 
-        }
+		}
 
-        return str.toString();
+		return str.toString();
 
-    }
+	}
 
+	public static void main(String[] args) {
 
+		ArrayList a = new ArrayList();
 
-    public static void main(String[] args) {
+		a.add(97750768);
+		a.add(60243460);
 
-        ArrayList a = new ArrayList();
+		LargestNumber largestNumber = new LargestNumber();
 
-        a.add(97750768);
-        a.add(60243460);
+		System.out.println(largestNumber.largestNumber(a));
 
-        LargestNumber largestNumber = new LargestNumber();
-
-        System.out.println(largestNumber.largestNumber(a));
-
-
-
-    }
-
+	}
 
 }
